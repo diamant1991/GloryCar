ff0569fd4762806b59103b6ea9c558b8a3eef134
diamant1991 <diamant1988@mail.ru>
2014-12-26 15:06:02 +0200

    change style main

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,18 @@
         $(".carousel-control").css("z-index","99");
         $('.form-search').css("z-index","99");
         $('.carousel-indicators').css("z-index","99");
+        $('.guide-href').css("z-index","99");
+
       },
       function(){
         $(".carousel-control").css("z-index","100");
         $('.form-search').css("z-index","100");
         $('.carousel-indicators').css("z-index","100");
+        $('.guide-href').css("z-index","100");
+      });
+
+      $('#counter').countdown({
+    timestamp : (new Date(2014, 12, 31)).getTime() + 01*01*01*60*1000
       });
       
     });
